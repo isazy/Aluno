3cc992793ffd35b74bdb6ba4a4ca65b358f6bcd1
isazy <146368490+isazy@users.noreply.github.com>
2024-02-01 15:37:53 -0300

    Add files via upload

---
+++ GerenciamentoDeDepartamentos/src/test/java/comGerenciamentoDeDepartamentos/GerenciamentoDeDepartamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package comGerenciamentoDeDepartamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeDepartamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
